3fefcca7c5458f3718df3513873d8b73aa79cb19
Matheus Mesquita Yukishita <161179335+math-ghub@users.noreply.github.com>
2024-03-23 16:40:02 -0400

    remake site equipe completo

--- estilos/equipe/script.js
+++ estilos/equipe/script.js
@@ -1,20 +1,33 @@
 const ImageList = document.getElementById("img-list")
 const infoValues = document.getElementById("info")
+const cargoVal = document.getElementById("portrait-cargo")
 var portraitButtons = document.getElementsByClassName("portrait-button")
 
 const Infos = {
-    "Michelle Mesquita": {
-        "text": "Aalo",
+    "Dra. Michelle Mesquita": {
+        "text": "• Proprietária Responsável Técnica Clínica Meridian Bem Estar <br>• Fisioterapeuta UNIC/2002 <br>• Pós graduada em Acunputura/2006 <br>• Dermatofuncional/2014 <br>• Cursos de Capacitação em RPG/2008 <br>• Pilates clássico/2011 <br>• Cross Pilates/2019",
+        "cargo": "Fundadora",
+        "conselho": "CREFITO 9- 54995-F",
     },
-    "Pessoa": {
-        "text": "Texto 2",
+    "Dra. Danyelle Padilha": {
+        "text": "Me chamo Danyelle Padilha, sou Nutricionista e Pós graduada em Nutrição e obesidade. Acredito que um dos primeiro pilares para qualidade de vida e principalmente, performance física, começa na nossa alimentação.<br> <br> Tendo a alimentação com base, podemos facilitar esse caminho entre ganho de massa magra e emagrecimento. Hoje o meu foco é principalmente esse, através de um protocolo simples e didático, compreendendo que cada paciente tem suas particularidades, conseguir proporcionar uma reeducação alimentar, e principalmente, saúde aos meus pacientes.<br> <br>E com o propósito de defender um estilo de vida saudável, sem terrorismos e com equilíbrio. Te convido a nos visitar, aqui na Clínica Meridian. Vamos falar da sua saúde?",
+        "cargo": "Nutricionista",
+        "conselho": "CRN 21700/P",
     },
-    "Pessoa 3": {
+    "Aline Lima": {
         "text": "Texto 3",
-    }
+        "cargo": "Esteticista",
+        "conselho": "none",
+    },
+    "Dra. Thamires Oliveira": {
+        "text": "4text",
+        "cargo": "Fisioterapeuta",
+        "conselho": "CREFITO 9- 320766-F",
+    },
 }
 
 let Selected = 0
+let oldTimeout = []
 
 portraitButtons["previous"].addEventListener("click", () => {
     Selected = Selected > 0 ? Selected - 1 : Object.keys(Infos).length - 1
@@ -30,8 +43,22 @@ function UpdatePortrait() {
     var selectedKey = Object.keys(Infos)[Selected]
     var selectedValues = Object.values(Infos)
     infoValues.children[0].innerHTML = selectedKey
-    infoValues.children[1].innerHTML = selectedValues[Selected].text
+    infoValues.children[1].innerHTML = selectedValues[Selected].conselho
+    Write(selectedValues[Selected].text, infoValues.children[2], 9)
+    cargoVal.children[0].innerHTML = selectedValues[Selected].cargo
     ImageList.style.transform = `translate(${-(Selected * 40) - 20}em, -50%)`
+}
+
+function Write(text, parent, ms) {
+    oldTimeout.forEach(val => {
+        clearTimeout(val)
+    })
+    textLength = text.length
+    for (let i = 0; i <= textLength; i++) {
+        oldTimeout.push(setTimeout(() => {
+            parent.innerHTML = text.substring(0, i)
+        }, ms * i))
+    }
 }
 
 UpdatePortrait()
