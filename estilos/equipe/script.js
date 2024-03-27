d6199fc588ddc116c85c3db394b69debb80efd45
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-03-27 17:15:17 -0300

    reformulacao fail

--- estilos/equipe/script.js
+++ estilos/equipe/script.js
@@ -1,3 +1,8 @@
+const ImageList = document.getElementById("img-list")
+const infoValues = document.getElementById("info")
+const cargoVal = document.getElementById("portrait-cargo")
+var portraitButtons = document.getElementsByClassName("portrait-button")
+
 const Infos = {
     "Dra. Michelle Mesquita": {
         "text": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Harum consequatur dolorum voluptates atque tempore odit quia impedit? Repellat, aspernatur non? Delectus facilis non reprehenderit fugit culpa sequi ullam molestias architecto.<br><br>• Proprietária Responsável Técnica Clínica Meridian Bem Estar <br>• Fisioterapeuta UNIC/2002 <br>• Pós graduada em Acunputura/2006 <br>• Dermatofuncional/2014 <br>• Cursos de Capacitação em RPG/2008 <br>• Pilates clássico/2011 <br>• Cross Pilates/2019",
@@ -20,3 +25,40 @@ const Infos = {
         "conselho": "CREFITO 9- 320766-F",
     },
 }
+
+let Selected = 0
+let oldTimeout = []
+
+portraitButtons["previous"].addEventListener("click", () => {
+    Selected = Selected > 0 ? Selected - 1 : Object.keys(Infos).length - 1
+    UpdatePortrait()
+})
+
+portraitButtons["next"].addEventListener("click", () => {
+    Selected = Selected < Object.keys(Infos).length - 1 ? Selected + 1 : 0
+    UpdatePortrait()
+})
+
+function UpdatePortrait() {
+    var selectedKey = Object.keys(Infos)[Selected]
+    var selectedValues = Object.values(Infos)
+    infoValues.children[0].innerHTML = selectedKey
+    infoValues.children[1].innerHTML = selectedValues[Selected].conselho
+    Write(selectedValues[Selected].text, infoValues.children[2], 7)
+    cargoVal.children[0].innerHTML = selectedValues[Selected].cargo
+    ImageList.style.transform = `translate(${-(Selected * 40) - 20}em, -50%)`
+}
+
+function Write(text, parent, ms) {
+    oldTimeout.forEach(val => {
+        clearTimeout(val)
+    })
+    textLength = text.length
+    for (let i = 0; i <= textLength; i++) {
+        oldTimeout.push(setTimeout(() => {
+            parent.innerHTML = text.substring(0, i)
+        }, ms * i))
+    }
+}
+
+UpdatePortrait()
