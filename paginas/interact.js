b0e532d80162d59f78cf6d89bc9a379c82a08426
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-03-29 16:07:45 -0400

    compactando codigo

--- paginas/interact.js
+++ paginas/interact.js
@@ -1,5 +1,4 @@
 var interativos = document.querySelectorAll(".interactive")
-var buttons = document.querySelectorAll(".button")
 var nav = document.querySelector("nav")
 var navButtons = document.querySelectorAll("nav > ul > li > a")
 var navSpecialButton = document.querySelector("nav li p")
@@ -33,20 +32,6 @@ nav.addEventListener("animationend", () => {
     if (nav.classList.contains("Closed")) {
     logo.style.zIndex = 1
 }})
-
-buttons.forEach(element => {
-    defaultbgColor = element.style.backgroundColor
-    defaultColor = element.style.color
-    element.addEventListener("mouseenter", () => {
-        element.style.backgroundColor = "rgb(34, 220, 90)"
-        element.style.color = "black"
-    })
-
-    element.addEventListener("mouseleave", () => {
-        element.style.backgroundColor = defaultbgColor
-        element.style.color = defaultColor
-    })
-});
 
 navSpecialButton.addEventListener("click", element => {
     if (window.innerWidth < 800) {
