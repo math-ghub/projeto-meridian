143f2776cdeb2c8b2a0585315ececab7eeee3a6d
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-03-29 14:25:46 -0400

    criado menu de servicos, começando a adaptar para mobile

--- paginas/interact.js
+++ paginas/interact.js
@@ -1,11 +1,12 @@
 var interativos = document.querySelectorAll(".interactive")
 var buttons = document.querySelectorAll(".button")
 var nav = document.querySelector("nav")
-var navButtons = document.querySelectorAll("nav li a")
+var navButtons = document.querySelectorAll("nav > ul > li > a")
 var navSpecialButton = document.querySelector("nav li p")
 var menuList = document.querySelector("#menuList")
 var menuButton = document.getElementById("menu")
 var servicoMenu = document.querySelector("#servicomenu")
+var categoryButtons = menuList.querySelectorAll("li")
 let insideMenuBar = false
 let insideMenu = false
 let logo = document.getElementById("logodiv")
@@ -47,14 +48,24 @@ buttons.forEach(element => {
     })
 });
 
-navSpecialButton.addEventListener("click", () => {
-    startLoad(element.target.parentElement, true)
-    displayMenu(true)
+navSpecialButton.addEventListener("click", element => {
+    if (window.innerWidth < 800) {
+        if (!menuList.classList.contains("openlist")) {
+            startLoad(element.target.parentElement, true)
+            displayMenu(true)
+        } else {
+            startLoad(element.target.parentElement, false)
+            displayMenu(false)
+            menuList.classList.remove("openlist")
+        }
+    }
 })
 
 navSpecialButton.addEventListener("mouseenter", element => {
-    startLoad(element.target.parentElement, true)
-    displayMenu(true)
+    if (window.innerWidth > 799) {
+        startLoad(element.target.parentElement, true)
+        displayMenu(true)
+    }
 })
 
 navSpecialButton.parentElement.addEventListener("mouseleave", element => {
@@ -114,6 +125,23 @@ startLoad = (element, bool) => {
         loadBar[0].classList.add("shrink")
     }
 }
+
+categoryButtons.forEach(button => {
+    let isOpen = false
+    button.addEventListener("click", () => {
+        let category = button.getElementsByClassName("categoria")[0].children[0]
+        let content = button.getElementsByClassName("conteudo")[0]
+        if (!isOpen) {
+            category.classList.add("rotate")
+            content.classList.add("show")
+            isOpen = true
+        } else {
+            category.classList.remove("rotate")
+            content.classList.remove("show")
+            isOpen = false
+        }
+    })
+})
 
 function checkHeight() {
     interativos.forEach(element => {
