829c3c97c931452b7139f4be0d9718b6fab63e1f
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-03-30 21:44:02 -0400

    sistema de transiçao de imagens da pagina inicial concluido

--- estilos/index/controller.js
+++ estilos/index/controller.js
@@ -1,39 +1,24 @@
 const frames = document.getElementsByClassName("e-fotos")
 const framesArray = Array.from(frames)
+let delay = 1
 let refreshTime = 2
 
 var images = [
     "estudio-teste.webp",
-    "estudio-teste.webp",
+    "clinica-interior.jpg",
 ]
 
 framesArray.forEach(box => {
     let actualImage = 0
-    setAnimation(box, actualImage)
-    box.addEventListener("animationstart", (anim) => {
-        if (anim.animationName.charAt(0) != "e") {return};
-        setTimeout(() => {
-            refresh(box)
-        },6000)
-    })
-    box.addEventListener("animationend", () => {
+    box.addEventListener("animationiteration", (anim) => {
+        if (anim.animationName == "refresh") {return};
         if (actualImage > images.length - 1) {actualImage = 0};
         setAnimation(box, actualImage)
         actualImage += 1
     })
+    setAnimation(box, actualImage)
 })
 
 function setAnimation(obj, val) {
-    obj.classList.remove("onLoop")
-    setTimeout(() => {
-        obj.style["background-image"] = "url('../../imagens/" + images[val] + "')"
-        obj.classList.add("onLoop")
-    }, 100)
-}
-
-function refresh(obj) {
-    obj.classList.add("refresh")
-    setTimeout(() => {
-        obj.classList.remove("refresh")
-    }, 1000 * refreshTime)
+    obj.style["background-image"] = "url('../../imagens/" + images[val] + "')"
 }
