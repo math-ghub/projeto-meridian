c5d7b85a0a10dd1b2dc944b4dd84703fb1ab7a62
math-ghub <161179335+math-ghub@users.noreply.github.com>
2024-04-01 22:35:44 -0400

    teste de videos como background e adiçao de imagem nova a secao estudio

--- estilos/index/controller.js
+++ estilos/index/controller.js
@@ -1,11 +1,9 @@
 const frames = document.getElementsByClassName("e-fotos")
 const framesArray = Array.from(frames)
-let delay = 1
-let refreshTime = 2
 
 var images = [
-    "estudio-teste.webp",
     "clinica-interior.jpg",
+    "clinica-interior2.png",
     "idosa-crosspilates.jpg",
 ]
 
@@ -13,7 +11,7 @@ framesArray.forEach(box => {
     let actualImage = 0
     box.addEventListener("animationiteration", (anim) => {
         if (anim.animationName == "refresh") {return actualImage += 1};
-        if (actualImage > images.length - 1) {actualImage = 0};
+        if (actualImage >= images.length) {actualImage = 0};
         setAnimation(box, actualImage)
     })
     setAnimation(box, actualImage)
