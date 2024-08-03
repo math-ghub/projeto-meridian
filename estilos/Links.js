0890cc3c491ba92ffab6fc1b49c3a25172435309
ykmath <matheusmesquitay@gmail.com>
2024-08-02 21:49:40 -0400

    adiçao de novos botoes pagina links / adiçao de texto pagina cursos

---
+++ estilos/Links.js
@@ -0,0 +1,21 @@
+const playListButton = document.querySelector("#i-playlist");
+const selectionMenu = document.querySelector("#selection-menu");
+const closeButton = document.querySelector("#selection-menu-close");
+let menuState = false;
+
+playListButton.addEventListener("click", () => {
+    setState(true);
+})
+
+closeButton.addEventListener("click", () => {
+    setState(false);
+})
+
+function setState(v) {
+    menuState = v;
+    if (menuState === false) {
+        selectionMenu.classList.remove("open");
+    } else {
+        selectionMenu.classList.add("open");
+    }
+}
